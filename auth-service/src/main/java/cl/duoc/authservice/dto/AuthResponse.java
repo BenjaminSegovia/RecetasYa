4f9ef4d933ba9d4661194e6db0a62a61a2e4f6f2
Benjamin Segovia <b.segovia.iccp@gmail.com>
2026-09-11 02:24:17 -0300

    (auth-service): Creacion de AuthResponse

---
+++ auth-service/src/main/java/cl/duoc/authservice/dto/AuthResponse.java
@@ -0,0 +1,17 @@
+package cl.duoc.authservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthResponse {
+    
+    private String token;
+    private String username;
+    private String role;
+}
