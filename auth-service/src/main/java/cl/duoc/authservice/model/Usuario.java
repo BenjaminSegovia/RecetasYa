9731aa3158ef929a8a806259451f35f34039a7f2
Benjamin Segovia <b.segovia.iccp@gmail.com>
2026-09-11 02:03:55 -0300

    Creacion de entidad Usuario

---
+++ auth-service/src/main/java/cl/duoc/authservice/model/Usuario.java
@@ -0,0 +1,33 @@
+package cl.duoc.authservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nombreCompleto;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
