8af5d6ce59be84f5a61785b6cd4ed1c9d039c057
Benjamin Segovia <b.segovia.iccp@gmail.com>
2026-09-11 02:07:09 -0300

    Creacion de UsuarioRepository

---
+++ auth-service/src/main/java/cl/duoc/authservice/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package cl.duoc.authservice.repository;
+
+import cl.duoc.authservice.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
