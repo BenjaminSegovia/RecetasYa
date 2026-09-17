980a47ce3d054881c88676237f4342458f2e805d
Benjamin Segovia <b.segovia.iccp@gmail.com>
2026-09-17 16:46:40 -0300

    (inventory-service): agregar StockController

---
+++ inventory-service/src/main/java/cl/duoc/inventoryservice/controller/StockController.java
@@ -0,0 +1,30 @@
+package cl.duoc.inventoryservice.controller;
+
+import cl.duoc.inventoryservice.dto.StockRequest;
+import cl.duoc.inventoryservice.dto.StockResponse;
+import cl.duoc.inventoryservice.service.StockService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/inventario")
+@RequiredArgsConstructor
+public class StockController {
+    
+    private final StockService stockService;
+
+    @PostMapping
+    public ResponseEntity<StockResponse> crearOActualizar(@Valid @RequestBody StockRequest request) {
+        return ResponseEntity.ok(stockService.crearOActualizarStock(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StockResponse>> listarTodo() {
+        return ResponseEntity.ok(stockService.listarTodo());
+    }
+
+}
